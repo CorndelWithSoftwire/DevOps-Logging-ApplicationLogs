1ad823f246e13d6bf5896234190627af68ded418
Alex Jones <alexander.jones@softwire.com>
2020-03-27 13:45:20 +0000

    Add logging config file

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
 import logging
+from flask import Flask
+from logging.config import fileConfig
+ 
+fileConfig('./logging.conf')
  
 app = Flask(__name__)
+
+@app.route('/')
+def homepage_with_info_log():
+    app.logger.info("Something not as serious")
+    return 'Logged Info Message'
  
 @app.route('/error_page')
 def log_an_error():
